9ee011452091fe84ad7a41302b2bf967a48e5b0a
Francisco J. Carriedo <fcarriedo@gmail.com>
2011-07-03 13:26:20 -0700

    Added comments to insertion sort algorithm.

--- sort/insertionsort/insertionsort.c
+++ sort/insertionsort/insertionsort.c
@@ -12,7 +12,7 @@ void insertionSort(int *array, int length){
     if(array[i+1] < array[i]) {
       int j=i+1;
       int tmp = array[j];
-      while(array[j-1] > tmp && j>0) {
+      while(array[j-1] > tmp && j>0) { // shift values 'till necessary.
         array[j] = array[j-1];
         j--;
       }
