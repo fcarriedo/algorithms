fa70dbecc366182c4fb1f604e199494ea16f19da
Francisco J. Carriedo <fcarriedo@gmail.com>
2011-07-03 00:39:45 -0700

    Added functionality to enter number as a command line argument. #fibonacci

--- recursive/fibonacci/fib.c
+++ recursive/fibonacci/fib.c
@@ -2,11 +2,27 @@
 
 int fib(int);
 
-int main() {
-  int number = 35;
+int main(int argc, char *argv[]) {
+  if(argc <= 1) {
+    printf("Usage: \n\t./fib my_number\n");
+    return 0;
+  }
+
+  int number = atoi(argv[1]);
+  if(!number) {
+    printf("'my_number' has to be an integer.\n");
+    return -1;
+  }
+
   printf("\nFibonacci # of %d: %d\n", number, fib(number));
 }
 
+/**
+ * Method that performs the fibonacci number calculation.
+ *
+ * Note: Its implementation is a beautiful recursive oneliner
+ * based on the ternary operator.
+ */
 int fib(int number) {
   return number<=1 ? number : fib(number-1) + fib(number-2);
 }
