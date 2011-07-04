b74ba1478bf4eb2e3036ce32518ec7caf1582234
Francisco J. Carriedo <fcarriedo@gmail.com>
2011-07-03 19:14:31 -0700

    Initial implementation if binary search. Note: buggy

--- search/binarysearch/binarysearch.c
+++ search/binarysearch/binarysearch.c
@@ -7,7 +7,20 @@ int binarySearch(int *, int, int, int);
  * existent.
  */
 int binarySearch(int *array, int initIx, int endIx, int elemToSearch) {
-  return 0;
+  int rangeLength = endIx - initIx;
+  printf("a[%d]=%d, a[%d]=%d, rangeLength: %d\n", initIx, array[initIx], endIx, array[endIx], rangeLength);
+  if(rangeLength <= 1) {
+    return array[initIx] == elemToSearch ? initIx : -1;
+  } else {
+    int halfIx = rangeLength/2;
+    if(array[halfIx] == elemToSearch) {
+      return halfIx;
+    } else if(elemToSearch < array[halfIx]) {
+      return binarySearch(array, initIx, halfIx, elemToSearch);
+    } else {
+      return binarySearch(array, halfIx+initIx, endIx, elemToSearch);
+    }
+  }
 }
 
 int main() {
@@ -19,6 +32,26 @@ int main() {
   printf("Index of searched elem %d = %d\n", elemToSearch, ix);
 
   elemToSearch = 60;
+  ix = binarySearch(sortedArray, 0, length-1, elemToSearch);
+  printf("Index of searched elem %d = %d\n", elemToSearch, ix);
+
+  elemToSearch = 0;
+  ix = binarySearch(sortedArray, 0, length-1, elemToSearch);
+  printf("Index of searched elem %d = %d\n", elemToSearch, ix);
+
+  elemToSearch = 9;
+  ix = binarySearch(sortedArray, 0, length-1, elemToSearch);
+  printf("Index of searched elem %d = %d\n", elemToSearch, ix);
+
+  elemToSearch = 23;
+  ix = binarySearch(sortedArray, 0, length-1, elemToSearch);
+  printf("Index of searched elem %d = %d\n", elemToSearch, ix);
+
+  elemToSearch = 92;
+  ix = binarySearch(sortedArray, 0, length-1, elemToSearch);
+  printf("Index of searched elem %d = %d\n", elemToSearch, ix);
+
+  elemToSearch = 93;
   ix = binarySearch(sortedArray, 0, length-1, elemToSearch);
   printf("Index of searched elem %d = %d\n", elemToSearch, ix);
 }
