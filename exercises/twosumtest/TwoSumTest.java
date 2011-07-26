bc6bbe5370b8130f5f278e0968123b2464c82878
Francisco Carriedo <fcarriedo@gmail.com>
2011-07-25 17:44:47 -0700

    Enhanced comment readability on TwoSumTest

--- exercises/twosumtest/TwoSumTest.java
+++ exercises/twosumtest/TwoSumTest.java
@@ -3,15 +3,20 @@ import java.util.*;
 interface TwoSum {
 
   /*
-   * Stores @param input in an internal data structure
+   * Stores the given number into an internal data structure
    */
   void store(int input);
 
   /*
-   * Returns true if there is any pair of numbers in the internal data structure which have sum @param test, and false otherwise
-   * For example, if the numbers 1, -2, 3, and 6 had been stored, the method should return true for 4, -1, and 9, but false for 10, 5, and 0
+   * Returns true if there is any pair of numbers in the internal
+   * data structure which have sum @param test,and false otherwise.
+   *
+   * Example: if the numbers 1, -2, 3, and 6 had been stored,
+   *          the method should return true for 4, -1, and 9,
+   *          but false for 10, 5, and 0.
    */
   boolean test (int test);
+
 }
 
 public class TwoSumTest {
