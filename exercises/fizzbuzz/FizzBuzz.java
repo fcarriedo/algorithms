1f1974c2f708a8dfd1a4acccc8856904f11249dd
Francisco Carriedo <fcarriedo@gmail.com>
2011-08-15 06:05:26 -0700

    Reduced FizzBuzz logic to one line with ternary operator.

--- exercises/fizzbuzz/FizzBuzz.java
+++ exercises/fizzbuzz/FizzBuzz.java
@@ -8,10 +8,7 @@
 public class FizzBuzz {
   public static void main(String[] args) {
     for(int i=1; i<=100; i++) {
-      if( i%3==0 && i%5==0 ) System.out.println("FizzBuzz");
-      else if( i%3==0 ) System.out.println("Fizz");
-      else if( i%5==0 ) System.out.println("Buzz");
-      else System.out.println( String.valueOf(i) );
+      System.out.println( i%3==0 && i%5==0 ? "FizzBuzz" : i%3==0 ? "Fizz" : i%5==0 ? "Buzz" : String.valueOf(i) );
     }
   }
 }
