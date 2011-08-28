c832c6e7cda75ab13093bf2a5b1f506fd653b24b
Francisco J. Carriedo <fcarriedo@gmail.com>
2011-08-28 00:40:18 -0700

    Made code more ~pythonic.

--- exercises/benford/benford-fib.py
+++ exercises/benford/benford-fib.py
@@ -13,38 +13,41 @@ from collections import defaultdict
 """
 
 def fib(n):
-  """Linear implementation of the fibonnaci number calculation"""
-  if n == 0 or n == 1: return n
-  curr, next = 1, 1
-  for x in xrange(2, n):
-    next, curr = ((next+curr),next)
-  return next
+    """
+    Linear (theoretical) implementation of the
+    fibonnaci number calculation
+    """
+    if n == 0 or n == 1: return n
+    curr, next = 1, 1
+    for x in xrange(2, n):
+        next, curr = ((next+curr),next)
+
+    return next
 
 def graph(stats,max_fib):
-  """Creates a simple ASCII graph"""
-  for i in range(1,10):
-    percent = (stats[str(i)]*100/max_fib)
-    percent_line = "".join('x' for x in range(percent))
-    print "\n%d = %d%% %s" % (i, percent, percent_line),
-
+    """Creates a simple ASCII graph"""
+    for i in range(1,10):
+        percent = (stats[str(i)]*100/max_fib)
+        graph_percent_line = "".join('x' for x in xrange(percent))
+        print "\n%d = %d%% %s" % (i, percent, graph_percent_line),
 
 def main(args):
-  if len(args) == 0:
-    print "\nYou need to specify a max fibonnaci number to calculate:"
-    print "\n   Usage: $ ./benford-fib.py 150\n"
-    sys.exit(1)
-  else:
-    max_fib = int(args[0])
+    if not args:
+        print "\nYou need to specify a max fibonnaci number to calculate:"
+        print "\n   Usage: $ ./benford-fib.py 150\n"
+        sys.exit(1)
+    else:
+        max_fib = int(args[0])
 
-  stats = defaultdict(int)
+    stats = defaultdict(int)
 
-  for i in range(max_fib):
-    fib_num = fib(i)
-    key = str(fib_num)[0:1]
-    stats[key] += 1
+    for i in xrange(max_fib):
+        fib_num = fib(i)
+        key = str(fib_num)[:1]
+        stats[key] += 1
 
-  graph(stats, max_fib)
+    graph(stats, max_fib)
 
 
 if __name__ == '__main__':
-  main(sys.argv[1:])
+    main(sys.argv[1:])
